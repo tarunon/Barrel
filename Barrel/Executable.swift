8bf4d067868c486d6b3f8c84dbdedd219121ce19
tarunon <croissant9603@gmail.com>
2015-06-17 10:54:24 +0900

    a little fix

--- Barrel/Executable.swift
+++ Barrel/Executable.swift
@@ -12,13 +12,12 @@ import CoreData
 public protocol Executable {
     typealias Type
     var context: NSManagedObjectContext { get }
-    func fetchRequest() -> NSFetchRequest
 }
 
-public extension Executable {
+public extension Executable where Self: Builder, Self.Result == NSFetchRequest {
     func all() throws -> [Type] {
         do {
-            let result = try context.executeFetchRequest(fetchRequest())
+            let result = try context.executeFetchRequest(builder())
             return result.map{ $0 as! Type }
         } catch let error {
             throw error
@@ -27,7 +26,7 @@ public extension Executable {
     
     func get() throws -> Type? {
         do {
-            let fetchRequest = self.fetchRequest()
+            let fetchRequest = builder()
             fetchRequest.fetchLimit = 1
             return try context.executeFetchRequest(fetchRequest).first as? Type
         } catch let error {
@@ -37,7 +36,7 @@ public extension Executable {
     
     func count() throws -> Int {
         var error: NSError?
-        let count = context.countForFetchRequest(fetchRequest(), error: &error)
+        let count = context.countForFetchRequest(builder(), error: &error)
         if let error = error {
             throw error
         }
