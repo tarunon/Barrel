51ac4fdd3be019522978f052cbb318a8f659e3cd
tarunon <croissant9603@gmail.com>
2016-11-22 17:12:48 +0900

    change easy to use KeyPath model

--- Core/KeyPath.swift
+++ Core/KeyPath.swift
@@ -8,28 +8,46 @@
 
 import Foundation
 
-public enum KeyPath {
+public indirect enum KeyPath {
     case `self`
     case keypath(String)
+    case subKeypath(String, KeyPath)
     
     internal init(_ name: String?, parent: KeyPath?) {
         guard let name = name else {
-            self = .`self`
+            self = .self
             return
         }
-        guard let parent = parent, case .keypath(let parentName) = parent else {
+        guard let parent = parent, parent != .self else {
             self = .keypath(name)
             return
         }
-        self = .keypath(parentName + "." + name)
+        self = .subKeypath(name, parent)
     }
     
     public var string: String {
         switch self {
-        case .`self`:
+        case .self:
             return "self"
         case .keypath(let keyPath):
             return keyPath
+        case .subKeypath(let keyPath, let parent):
+            return parent.string + "." + keyPath
+        }
+    }
+}
+
+extension KeyPath: Equatable {
+    public static func == (lhs: KeyPath, rhs: KeyPath) -> Bool {
+        switch (lhs, rhs) {
+        case (.self, .self):
+            return true
+        case (.keypath(let lKeypath), .keypath(let rKeypath)):
+            return lKeypath == rKeypath
+        case (.subKeypath(let lKeypath, let lParent), .subKeypath(let rKeypath, let rParent)):
+            return lKeypath == rKeypath && lParent == rParent
+        default:
+            return false
         }
     }
 }
